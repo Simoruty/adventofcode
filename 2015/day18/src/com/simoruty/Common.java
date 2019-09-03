e3bc23207d6811a249ffe50dfc1312897628b1ee
Simone Rutigliano <simonerutigliano88@gmail.com>
2019-09-03 10:28:32 +0200

    Task completed

--- 2015/day18/src/com/simoruty/Common.java
+++ 2015/day18/src/com/simoruty/Common.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 class Common {
 
-    static ArrayList<String> readFromFile(String path) {
+    private static ArrayList<String> readFromFile(String path) {
         ArrayList<String> lines = new ArrayList<>();
         try {
 
@@ -24,5 +24,72 @@ class Common {
             e.printStackTrace();
         }
         return lines;
+    }
+
+    static boolean[][] createMatrix(String path) {
+        ArrayList<String> input = Common.readFromFile(path);
+        int maxRow = input.get(0).length();
+        int maxCol = input.size();
+
+        boolean[][] matrix = new boolean[maxRow][maxCol];
+
+        int curRow = 0;
+        for (String s : input) {
+            for (int i = 0; i < s.length(); i++) matrix[curRow][i] = s.charAt(i) == '#';
+            curRow++;
+        }
+        return matrix;
+    }
+
+    static void printMatrix(boolean[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j]) System.out.print("# ");
+                else System.out.print(". ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int lights(boolean[][] matrix) {
+        int lights = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j]) lights++;
+            }
+        }
+        return lights;
+    }
+
+    static boolean[][] fixCornerMatrix(boolean[][] matrix) {
+        matrix[0][0] = true;
+        matrix[0][matrix.length - 1] = true;
+        matrix[matrix[0].length - 1][0] = true;
+        matrix[matrix[0].length - 1][matrix.length - 1] = true;
+        return matrix;
+    }
+
+    static boolean neighbors(boolean[][] matrix, int indexRow, int indexCol, boolean corner) {
+
+        if (corner &&
+                (indexCol == 0 && indexRow == 0) ||
+                (indexCol == 0 && indexRow == matrix[0].length - 1) ||
+                (indexCol == matrix.length - 1 && indexRow == 0) ||
+                (indexCol == matrix.length - 1 && indexRow == matrix[0].length - 1))
+            return true;
+
+        int countOn = 0;
+        for (int i = indexRow - 1; i <= indexRow + 1; i++) {
+            for (int j = indexCol - 1; j <= indexCol + 1; j++) {
+                if ((i >= 0 && i < matrix.length) && (j >= 0 && j < matrix[0].length)) {
+                    if (matrix[i][j] && !(i == indexRow && j == indexCol)) countOn++;
+                }
+            }
+        }
+
+        //System.out.println(" La cella è " + indexRow + " col: " + indexCol + " è " + matrix[indexRow][indexCol] + " count: "+countOn);
+        if (matrix[indexRow][indexCol]) {
+            return countOn == 2 || countOn == 3;
+        } else return countOn == 3;
     }
 }
--- 2015/day18/src/com/simoruty/Main.java
+++ 2015/day18/src/com/simoruty/Main.java
@@ -1,14 +1,27 @@
 package com.simoruty;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> input = Common.readFromFile("./input");
-        for (String s : input) {
-            System.out.println(s);
+        boolean[][] matrix = Common.createMatrix("./input");
+        //System.out.println("INIT STATE");
+        //Common.printMatrix(matrix);
+
+        Common.fixCornerMatrix(matrix);
+
+        for (int z = 1; z <= 100; z++) {
+            boolean[][] newMatrix = new boolean[matrix.length][matrix[0].length];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    newMatrix[i][j] = Common.neighbors(matrix, i, j, true);
+                }
+            }
+            matrix = newMatrix;
+            //System.out.println("STEP " + z);
+            //Common.printMatrix(matrix);
         }
 
+        System.out.println("Lights is " + Common.lights(matrix));
     }
+
 }

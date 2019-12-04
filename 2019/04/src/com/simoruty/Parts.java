5e28b06e8748a7e5a9bdd3ea06b0c514ffe7cd04
Simone Rutigliano <simonerutigliano88@gmail.com>
2019-12-04 20:05:45 +0100

    Update Parts.java
    Fixed parts 2

--- 2019/04/src/com/simoruty/Parts.java
+++ 2019/04/src/com/simoruty/Parts.java
@@ -38,7 +38,6 @@ public class Parts {
         return true;
     }
 
-
     private static boolean check2(int pwd) {
         char[] chars = ("" + pwd).toCharArray();
 
@@ -49,25 +48,25 @@ public class Parts {
         //Two adjacent digits are the same
         char top = chars[0];
         char tocheck = Character.MAX_VALUE;
-        boolean checkDouble = false;
+        int checkDouble = 0;
         boolean checkTriple = false;
         for (int i = 1; i < chars.length; i++) {
             char c = chars[i];
-            if(checkTriple && c != tocheck)
-                return false;
-            else if (checkTriple)
-                checkTriple = false;
-            else if (c == tocheck)
+            if(checkTriple && c == tocheck)
+                checkTriple= false;
+            else if (c == tocheck) {
                 checkTriple = true;
+                checkDouble --;
+            }
             else if (top == c) {
                 tocheck = top;
-                checkDouble = true;
+                checkTriple = false;
+                checkDouble ++;
             }
-
             top = chars[i];
         }
 
-        if (!checkDouble || checkTriple) return false;
+        if (checkDouble<1) return false;
 
         //Going from left to right, the digits never decrease; they only ever increase or stay the same
         for (int j=0; j<chars.length-1; j++) {
@@ -77,7 +76,7 @@ public class Parts {
         //System.out.println("True: " + String.copyValueOf(chars));
         return true;
     }
-
+   
     public static void part2(Integer[] rangePwd) {
 
         System.out.println("---------------- PART 2 -------------------");

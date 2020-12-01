6ce8601ef2d59d217d16b59e67258f9debb2bb6b
Simone Rutigliano <simonerutigliano88@gmail.com>
2020-12-01 17:43:25 +0100

    Day 1 - 2020

---
+++ 2020/01/src/com/simoruty/aoc2020/Common.java
@@ -0,0 +1,20 @@
+package com.simoruty.aoc2020;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Common {
+    static ArrayList<Integer> readFromFile(String path) {
+        Scanner scanner;
+        try {
+            scanner = new Scanner(new File(path));
+            ArrayList<Integer> list = new ArrayList<>();
+            while (scanner.hasNextInt()) list.add(scanner.nextInt());
+            return list;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

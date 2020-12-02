c8d87e169277eb73506b00e56d0d70b3237ce4c1
srutigliano <simone.rutigliano@it.ibm.com>
2020-12-02 14:35:40 +0100

    Day 1 - 2020

--- 2020/01/src/com/simoruty/aoc2020/Main.java
+++ 2020/01/src/com/simoruty/aoc2020/Main.java
@@ -7,16 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Integer> values = Common.readFromFile("./test/input.txt");
-        Collections.sort(values);
         part1(values);
         part2(values);
     }
 
     private static void part1(ArrayList<Integer> values) {
         for (int i = 0; i < values.size(); i++) {
-            for (int j = values.size()-1; j > i; j--) {
-                if (values.get(i)+values.get(j)==2020)
+            for (int j=i+1; j< values.size()-1; j++) {
+                if (values.get(i)+values.get(j)==2020){
                     System.out.println(values.get(i) + " * "+values.get(j)+" = " + values.get(i)*values.get(j));
+                    return;
+                }
             }
         }
     }
